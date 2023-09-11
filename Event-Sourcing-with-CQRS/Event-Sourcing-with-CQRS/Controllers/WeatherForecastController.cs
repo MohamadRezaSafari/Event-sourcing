f22d64c06d391ad4abc2e48bf58d638785b65494
Reza <Reza@DESKTOP-CKL538G>
2023-09-11 16:26:44 +0330

    Initial

---
+++ Event-Sourcing-with-CQRS/Event-Sourcing-with-CQRS/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Sourcing_with_CQRS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+    }
+}
